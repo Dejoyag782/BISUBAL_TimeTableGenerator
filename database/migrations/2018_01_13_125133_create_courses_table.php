3b3352478651d864767174f1cdca88f8bb106e2c
dejoyag782 <dejoyag782@gmail.com>
2024-03-06 22:06:55 +0800

    Added room_preference column based on the room ids

--- database/migrations/2018_01_13_125133_create_courses_table.php
+++ database/migrations/2018_01_13_125133_create_courses_table.php
@@ -17,6 +17,7 @@ class CreateCoursesTable extends Migration
             $table->increments('id');
             $table->string('course_code');
             $table->string('name');
+            $table->integer('room_preference')->nullable()->unsigned();
             $table->timestamps();
         });
     }
