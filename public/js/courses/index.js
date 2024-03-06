6bff69e3ca1b141c5370b862865900c7a1a71080
dejoyag782 <dejoyag782@gmail.com>
2024-03-06 22:10:41 +0800

    Added a script for the room_preference select/option area for displaying it only if course_code contains lab

--- public/js/courses/index.js
+++ public/js/courses/index.js
@@ -8,12 +8,40 @@ function Course(url, resourceName) {
 App.extend(Resource, Course);
 
 Course.prototype.prepareForUpdate = function (resource) {
+
+    var test = resource.course_code;
+    var testlowerCase = test.toLowerCase();
+    console.log(test);
     $('input[name=name]').val(resource.name);
     $('input[name=course_code]').val(resource.course_code);
     $('#professors-select').val(resource.professor_ids).change();
+    $('#room-select').val(resource.room_preference).change();
+    if(testlowerCase.includes('lab')){
+        roomDiv.removeAttribute('hidden');
+        $('#room-select').val(resource.room_preference).change();
+    }else{
+        roomDiv.setAttribute('hidden', 'true');
+        $('#room-select').val("").change();
+    }
 };
 
 window.addEventListener('load', function () {
     var course= new Course('/courses', 'Course');
     course.init();
+});
+
+var roomDiv = document.getElementById('roomDiv');
+
+document.getElementById('course_code').addEventListener('input', function() {
+    var courseCode = this.value.toLowerCase();
+    if (courseCode.includes('lab')) {
+        roomDiv.removeAttribute('hidden');
+    } else {
+        roomDiv.setAttribute('hidden', 'true');
+    }
+});
+
+document.getElementById('cancel_resource_modal').addEventListener('click', function(){
+    $('#room-select').val("").change();
+    roomDiv.setAttribute('hidden', 'true');
 });
