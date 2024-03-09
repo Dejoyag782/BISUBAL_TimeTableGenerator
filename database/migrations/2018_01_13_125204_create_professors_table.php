d1207b265130e929ae22c1b86f4644e44fd82a5d
dejoyag782 <dejoyag782@gmail.com>
2024-03-10 02:10:29 +0800

    Added departments column to professors table schema

--- database/migrations/2018_01_13_125204_create_professors_table.php
+++ database/migrations/2018_01_13_125204_create_professors_table.php
@@ -16,6 +16,7 @@ class CreateProfessorsTable extends Migration
         Schema::create('professors', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('department')->unsigned()->nullable();
             $table->timestamps();
         });
     }
