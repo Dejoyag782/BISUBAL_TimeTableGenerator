daf6e012b2c348378b2f6a693c5342d8018846a8
dejoyag782 <dejoyag782@gmail.com>
2024-03-29 20:52:53 +0800

    Added a checker for department deans and chairperson in order to generate timetables to avoid failure in rendering

--- app/Services/TimetableService.php
+++ app/Services/TimetableService.php
@@ -7,6 +7,7 @@ use App\Models\Timetable;
 use App\Models\Professor;
 use App\Models\CollegeClass;
 use App\Models\Department;
+use App\Models\User;
 
 use Illuminate\Support\Facades\DB;
 
@@ -24,6 +25,34 @@ class TimetableService
 
         $coursesQuery = 'SELECT id FROM courses WHERE id NOT IN (SELECT DISTINCT course_id FROM courses_professors)';
         $courseIds = DB::select($coursesQuery);
+        // Check if there is a campus director
+        $campusDirector = User::where('designation', 'campusdirector')->first();
+
+        // Check for Deans and Chairpersons for each department
+        $departments = Department::all();
+        foreach ($departments as $department) {
+            $dean = User::where('department', $department->id)->where('designation', 'dean')->first();
+            $chairperson = User::where('department', $department->id)->where('designation', 'chairperson')->first();
+            
+            if (!$dean) {
+                $errors[] = "No Dean assigned for the department: {$department->name}.";
+            }
+            if ($dean) {
+                // $errors[] = "No Dean assigned for the department: {$department->name}.";
+                continue;
+            }
+            if (!$chairperson) {
+                $errors[] = "No Chairperson assigned for the department: {$department->name}.";
+            }
+            if ($chairperson) {
+                // $errors[] = "No Chairperson assigned for the department: {$department->name}.";
+                continue;
+            }
+        }
+
+        if (!$campusDirector) {
+            $errors[] = "Campus Director not found. Please contact System Administrator to assign a Campus Director.";
+        }
 
         if (count($courseIds)) {
             $errors[] = "Some subjects don't have professors.<a href=\"/courses?filter=no_professor\" target=\"_blank\">Click here to review them</a>";
